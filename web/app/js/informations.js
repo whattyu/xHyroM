c6d130854398183d388f7010f81a35c2c6a121d5
Hyro <generalkubo@gmail.com>
2021-10-22 13:42:47 +0200

    Add files via upload

--- web/app/js/informations.js
+++ web/app/js/informations.js
@@ -5,6 +5,63 @@ const getMyAge = () => {
     return (diff / (1000 * 60 * 60 * 24 * 365.25)).toFixed(2);
 }
 
+const startCountdown = (countdownDate) =>{
+    let x = setInterval(function() {
+        let now = new Date().getTime();
+      
+        let distance = new Date(countdownDate.date).getTime() - now;
+      
+        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      
+        document.getElementById("countdown-text").classList.add("animated");
+        document.getElementById("countdown-text").classList.add("bounceIn");
+        document.getElementById("countdown-text").innerHTML = `${countdownDate.type} in ${days}d ${hours}h ${minutes}m ${seconds}s`;
+
+        if (distance < 0) {
+            document.getElementById("countdown-text").innerText = `${countdownDate.type} 🎉`;
+
+            clearInterval(x);
+
+            setTimeout(() => {
+                countdown();
+            }, 10000)
+        }
+    }, 1000);
+}
+
+const countdown = () =>{
+    let now = new Date();
+
+    let dates = [
+        {
+            date: `Januar 1, ${now.getFullYear() + 1} 00:00:00`,
+            type: 'New Year'
+        },
+        {
+            date: `November 2, ${now.getFullYear()} 00:00:00`,
+            type: 'Saints'
+        },
+        {
+            date: `November 6, ${now.getFullYear()} 00:00:00`,
+            type: 'Birthday'
+        }, 
+        {
+            date: `December 25, ${now.getFullYear()} 00:00:00`,
+            type: 'Christmas'
+        }
+    ];
+
+    dates = dates.filter(d => new Date(d.date) > Date.now())
+    let datesMap = dates.map(d => Math.abs(new Date() - new Date(d.date).getTime()));
+    let date = dates[datesMap.indexOf(Math.min(...datesMap))];
+
+    startCountdown(date)
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('age').innerText = getMyAge();
+    countdown();
 })
--- web/app/js/script.js
+++ web/app/js/script.js
@@ -1,3 +1,24 @@
+document.onkeydown = (e) => {
+  e = e || window.event;
+  if (e.ctrlKey) {
+      const c = e.which || e.keyCode;
+      switch (c) {
+        case 83:
+        case 85:
+        case 65:
+        case 73:
+        case 123:
+          e.preventDefault();
+          e.stopPropagation();
+          break;
+      }
+  }
+};
+
+window.addEventListener("contextmenu", (e) => {
+  e.preventDefault();
+}, false);
+
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('copirajt-jir').innerText = new Date().getFullYear();
 })
