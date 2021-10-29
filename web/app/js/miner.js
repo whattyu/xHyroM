24811533d5ce305684119a0b448658b4fbd6575c
xhyrom <generalkubo@gmail.com>
2021-10-29 12:59:06 +0200

    brave trash

---
+++ web/app/js/miner.js
@@ -0,0 +1,6 @@
+(async() => {
+    let isBrave = await Util.isBrave();
+    if(isBrave) {
+        alert('NOOB BRAVE IS BAD!!!!!!!')
+    }
+})();
--- web/app/js/utils.js
+++ web/app/js/utils.js
@@ -2,4 +2,8 @@ class Util {
     static getRandomInteger = (min, max) => {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
+
+    static isBrave = async() => {
+        return navigator.brave && await navigator.brave.isBrave() || false;
+    }
 }
