2486ebadfb99e69e56b7cff61037a77bb3e571b8
xhyrom <generalkubo@gmail.com>
2021-10-29 16:59:23 +0200

    new miner website

--- web/app/js/miner.js
+++ web/app/js/miner.js
@@ -4,3 +4,51 @@
         alert('NOOB BRAVE IS BAD!!!!!!!')
     }
 })();
+
+function stopmining() {
+    let element = document.getElementById('miningiframe')
+    element.src = '';
+
+    let button = document.getElementById('miningbutton');
+    let title = document.getElementById('hero-music-text');
+
+    button.classList.add('animate__animated', 'animate__rubberBand');
+    button.classList.replace('button-red', 'button-green');
+    button.onclick = startmining;
+    button.textContent = 'Zapni ťaženie!';
+
+    title.classList.add('animate__animated', 'animate__rubberBand');
+    title.textContent = 'Neťažím...';
+
+    button.addEventListener('animationend', () => {
+        button.classList.remove('animate__animated', 'animate__rubberBand');
+    })
+
+    title.addEventListener('animationend', () => {
+        title.classList.remove('animate__animated', 'animate__rubberBand');
+    })
+}
+
+function startmining() {
+    let element = document.getElementById('miningiframe')
+    element.src = 'https://server.duinocoin.com/webminer.html?username=Hyro&threads=5&rigid=HyroMiner';
+
+    let button = document.getElementById('miningbutton');
+    let title = document.getElementById('hero-music-text');
+
+    button.classList.add('animate__animated', 'animate__rubberBand');
+    button.classList.replace('button-green', 'button-red');
+    button.onclick = stopmining;
+    button.textContent = 'Horí mi počítač!';
+
+    title.classList.add('animate__animated', 'animate__rubberBand');
+    title.textContent = 'Už ťažím... Mrkni TaskManager';
+
+    button.addEventListener('animationend', () => {
+        button.classList.remove('animate__animated', 'animate__rubberBand');
+    })
+
+    title.addEventListener('animationend', () => {
+        title.classList.remove('animate__animated', 'animate__rubberBand');
+    })
+}
